093d639164be689af9a6259ddd8340916ab4aa26
Martin Atkins <mart@degeneration.co.uk>
2025-02-23 08:42:06 -0800

    Initial definitions for RISCovite

---
+++ src/riscovite.rs
@@ -0,0 +1,122 @@
+//! RISCovite libc definitions
+
+use crate::prelude::*;
+
+pub type intmax_t = i64;
+pub type uintmax_t = u64;
+
+pub type size_t = usize;
+pub type ptrdiff_t = isize;
+pub type intptr_t = isize;
+pub type uintptr_t = usize;
+pub type ssize_t = isize;
+
+pub type off_t = i64;
+pub type wchar_t = u32;
+
+pub const INT_MIN: c_int = -2147483648;
+pub const INT_MAX: c_int = 2147483647;
+
+pub type time_t = i64;
+
+pub type pthread_t = c_ulong;
+pub type pthread_key_t = c_ulong;
+
+s! {
+    pub struct timespec {
+        pub tv_sec: time_t,
+        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
+        pub tv_nsec: i64,
+        #[cfg(not(all(target_arch = "x86_64", target_pointer_width = "32")))]
+        pub tv_nsec: c_long,
+    }
+    pub struct pthread_attr_t {
+        // TODO
+    }
+    pub struct pthread_mutex_t {
+        // TODO
+    }
+    pub struct pthread_mutexattr_t {
+        // TODO
+    }
+    pub struct pthread_cond_t {
+        // TODO
+    }
+    pub struct pthread_condattr_t {
+        // TODO
+    }
+}
+
+extern "C" {
+    pub fn abort() -> !;
+    pub fn exit(status: c_int) -> !;
+    pub fn _exit(status: c_int) -> !;
+    pub fn strerror(num: c_int) -> *const c_char;
+
+    pub fn calloc(nobj: size_t, size: size_t) -> *mut c_void;
+    pub fn malloc(size: size_t) -> *mut c_void;
+    pub fn realloc(p: *mut c_void, size: size_t) -> *mut c_void;
+    pub fn free(p: *mut c_void);
+    pub fn memalign(align: size_t, size: size_t) -> *mut c_void;
+    pub fn posix_memalign(memptr: *mut *mut c_void, align: size_t, size: size_t) -> c_int;
+    pub fn close(fd: c_int) -> c_int;
+    pub fn strlen(cs: *const c_char) -> size_t;
+    pub fn getauxval(type_: c_ulong) -> c_ulong;
+    pub fn mmap(
+        addr: *mut c_void,
+        len: size_t,
+        prot: c_int,
+        flags: c_int,
+        fd: c_int,
+        offset: off_t,
+    ) -> *mut c_void;
+    pub fn munmap(addr: *mut c_void, len: size_t) -> c_int;
+
+    // RISCovite libc provides a pthread implementation that does thread
+    // switching in userspace.
+    pub fn pthread_self() -> crate::pthread_t;
+    pub fn pthread_equal(t1: crate::pthread_t, t2: crate::pthread_t) -> c_int;
+    pub fn pthread_join(native: crate::pthread_t, value: *mut *mut c_void) -> c_int;
+    pub fn pthread_exit(value: *mut c_void) -> !;
+    pub fn pthread_attr_init(attr: *mut crate::pthread_attr_t) -> c_int;
+    pub fn pthread_attr_destroy(attr: *mut crate::pthread_attr_t) -> c_int;
+    pub fn pthread_attr_getstacksize(
+        attr: *const crate::pthread_attr_t,
+        stacksize: *mut size_t,
+    ) -> c_int;
+    pub fn pthread_attr_setstacksize(attr: *mut crate::pthread_attr_t, stack_size: size_t)
+        -> c_int;
+    pub fn pthread_attr_setdetachstate(attr: *mut crate::pthread_attr_t, state: c_int) -> c_int;
+    pub fn pthread_detach(thread: crate::pthread_t) -> c_int;
+    pub fn sched_yield() -> c_int;
+    pub fn pthread_key_create(
+        key: *mut pthread_key_t,
+        dtor: Option<unsafe extern "C" fn(*mut c_void)>,
+    ) -> c_int;
+    pub fn pthread_key_delete(key: pthread_key_t) -> c_int;
+    pub fn pthread_getspecific(key: pthread_key_t) -> *mut c_void;
+    pub fn pthread_setspecific(key: pthread_key_t, value: *const c_void) -> c_int;
+    pub fn pthread_mutex_init(
+        lock: *mut pthread_mutex_t,
+        attr: *const pthread_mutexattr_t,
+    ) -> c_int;
+    pub fn pthread_mutex_destroy(lock: *mut pthread_mutex_t) -> c_int;
+    pub fn pthread_mutex_lock(lock: *mut pthread_mutex_t) -> c_int;
+    pub fn pthread_mutex_trylock(lock: *mut pthread_mutex_t) -> c_int;
+    pub fn pthread_mutex_unlock(lock: *mut pthread_mutex_t) -> c_int;
+    pub fn pthread_mutexattr_init(attr: *mut pthread_mutexattr_t) -> c_int;
+    pub fn pthread_mutexattr_destroy(attr: *mut pthread_mutexattr_t) -> c_int;
+    pub fn pthread_mutexattr_settype(attr: *mut pthread_mutexattr_t, _type: c_int) -> c_int;
+    pub fn pthread_cond_init(cond: *mut pthread_cond_t, attr: *const pthread_condattr_t) -> c_int;
+    pub fn pthread_cond_wait(cond: *mut pthread_cond_t, lock: *mut pthread_mutex_t) -> c_int;
+    pub fn pthread_cond_timedwait(
+        cond: *mut pthread_cond_t,
+        lock: *mut pthread_mutex_t,
+        abstime: *const crate::timespec,
+    ) -> c_int;
+    pub fn pthread_cond_signal(cond: *mut pthread_cond_t) -> c_int;
+    pub fn pthread_cond_broadcast(cond: *mut pthread_cond_t) -> c_int;
+    pub fn pthread_cond_destroy(cond: *mut pthread_cond_t) -> c_int;
+    pub fn pthread_condattr_init(attr: *mut pthread_condattr_t) -> c_int;
+    pub fn pthread_condattr_destroy(attr: *mut pthread_condattr_t) -> c_int;
+}
